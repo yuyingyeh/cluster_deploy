f8f078190707e81297daa5439aa7bac7cae1b41b
yuyingyeh <yuying1423@gmail.com>
2020-11-09 02:30:47 -0800

    update runRenderPredAll.sh

--- runRenderPredAll.sh
+++ runRenderPredAll.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt-get update
-apt-get install libopencv-dev
+apt-get -y install libopencv-dev
 cd /siggraphasia20dataset/code/Routine/DatasetCreation/material
 python renderImgMatPred.py --sceneId 0001_00 --rgbMode im --matMode cs
 python renderImgMatPred.py --sceneId 0001_00 --rgbMode imscannet --matMode cs
