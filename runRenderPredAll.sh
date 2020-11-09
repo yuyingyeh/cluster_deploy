b29698941b2e268b672ce0fc33d76205264c213f
yuyingyeh <yuying1423@gmail.com>
2020-11-09 02:43:38 -0800

    update runRenderPredAll.sh

--- runRenderPredAll.sh
+++ runRenderPredAll.sh
@@ -12,4 +12,4 @@ python renderImgMatPred.py --sceneId 0001_00 --rgbMode imscannet --matMode w
 python renderImgMatPred.py --sceneId 0028_00 --rgbMode im --matMode w
 python renderImgMatPred.py --sceneId 0028_00 --rgbMode imscannet --matMode w
 pip3 install opencv-python==3.4.2.17
-python3 fromHDRtoLDR.py
+python3 fromHDRtoLDR_All.py
