9df22fed8df3cff8985a0fca7163556b4bf92d2c
yuyingyeh <yuying1423@gmail.com>
2020-11-09 02:25:45 -0800

    add runRenderPredAll.sh

---
+++ runRenderPredAll.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+apt-get update
+apt-get install libopencv-dev
+cd /siggraphasia20dataset/code/Routine/DatasetCreation/material
+python renderImgMatPred.py --sceneId 0001_00 --rgbMode im --matMode cs
+python renderImgMatPred.py --sceneId 0001_00 --rgbMode imscannet --matMode cs
+python renderImgMatPred.py --sceneId 0028_00 --rgbMode im --matMode cs
+python renderImgMatPred.py --sceneId 0028_00 --rgbMode imscannet --matMode cs
+python renderImgMatPred.py --sceneId 0001_00 --rgbMode im --matMode w
+python renderImgMatPred.py --sceneId 0001_00 --rgbMode imscannet --matMode w
+python renderImgMatPred.py --sceneId 0028_00 --rgbMode im --matMode w
+python renderImgMatPred.py --sceneId 0028_00 --rgbMode imscannet --matMode w
