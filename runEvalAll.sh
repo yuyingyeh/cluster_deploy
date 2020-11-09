a4b211823508c1892611952ce55a9abbe63ebffc
yuyingyeh <yuying1423@gmail.com>
2020-11-09 02:01:07 -0800

    add runEvalAll.sh

---
+++ runEvalAll.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+pip3 install tqdm ipython opencv-python==3.4.2.17 tensorboard==1.15.0
+pip3 install torch --upgrade
+cd /siggraphasia20dataset/adobe_svbrdf-master
+python3 script_evalEncoder.py --sceneId 0001_00 --rgbMode im
+python3 script_evalEncoder.py --sceneId 0001_00 --rgbMode imscannet
+python3 script_evalEncoder.py --sceneId 0028_00 --rgbMode im
+python3 script_evalEncoder.py --sceneId 0028_00 --rgbMode imscannet
+python3 script_evalW.py --sceneId 0001_00 --rgbMode im
+python3 script_evalW.py --sceneId 0001_00 --rgbMode imscannet
+python3 script_evalW.py --sceneId 0028_00 --rgbMode im
+python3 script_evalW.py --sceneId 0028_00 --rgbMode imscannet
