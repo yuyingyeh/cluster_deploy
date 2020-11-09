bb26ae0dae5dd9adb5f49c012e2ea2c3bb7aa308
yuyingyeh <yuying1423@gmail.com>
2020-11-09 04:06:31 -0800

    update runRenderPred.sh

--- runRenderPred.sh
+++ runRenderPred.sh
@@ -3,6 +3,6 @@
 apt-get update
 apt-get -y install libopencv-dev
 cd /siggraphasia20dataset/code/Routine/DatasetCreation/material
-python renderImgMatPred.py --sceneId $1 --rgbMode $2 --matMode $3
+python renderImgMatPred.py --sceneId $1 --rgbMode $2 --matMode $3 --forceOutput
 pip3 install tqdm opencv-python==3.4.2.17
 python3 fromHDRtoLDR.py --sceneId $1 --rgbMode $2 --matMode $3
