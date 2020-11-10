000897edf5a7de338ef48ab879b516b189174573
yuyingyeh <yuying1423@gmail.com>
2020-11-09 20:23:36 -0800

    modify runRenderScannetViewData.sh

--- runRenderScannetViewData.sh
+++ runRenderScannetViewData.sh
@@ -4,5 +4,7 @@ apt-get update
 apt-get -y install libopencv-dev
 cd /siggraphasia20dataset/code/Routine/DatasetCreation/material
 python renderImgSN.py --sceneId $1 --forceOutput
+python renderDepthSN.py --sceneId $1 --forceOutput
+python renderMatPartSN.py --sceneId $1 --forceOutput
 pip3 install tqdm opencv-python==3.4.2.17
 python3 fromHDRtoLDR.py --sceneId $1 --rgbMode imscannet --matMode _
