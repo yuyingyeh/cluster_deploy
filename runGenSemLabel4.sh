7345d4cf2d7459ccf1bb9088402ed5caa84d2eae
yuyingyeh <yuying1423@gmail.com>
2020-11-13 18:35:27 -0800

    update runGenSemLabel4.sh

--- runGenSemLabel4.sh
+++ runGenSemLabel4.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pip3 install tqdm opencv-python==3.4.2.17
+#pip3 install tqdm opencv-python==3.4.2.17
 cd /siggraphasia20dataset/code/Routine/DatasetCreation/material
-python3 genSemanticLabel4.py --dirId $1 --phase $2 --sceneSplit $3
+/siggraphasia20dataset/anaconda3/bin/python3 genSemanticLabel4.py --dirId $1 --phase $2 --sceneSplit $3
