5db8c620904a90f9a152c8716edcd471aea9bdf5
yuyingyeh <yuying1423@gmail.com>
2021-02-23 04:46:27 -0800

    add runRenderStatSL.sh

--- runRenderStatSL.sh
+++ runRenderStatSL.sh
@@ -3,8 +3,8 @@
 apt-get update
 apt-get -y install libopencv-dev
 pip3 install tqdm opencv-python==3.4.2.17
-mkdir -p /eccv20dataset/yyeh/OpenRoomScanNetView/scene$1/renderUV
-cd /eccv20dataset/yyeh/OpenRoomScanNetView/scene$1/renderUV
+mkdir -p /eccv20dataset/yyeh/OpenRoomScanNetView/scene$1/renderUVSL
+cd /eccv20dataset/yyeh/OpenRoomScanNetView/scene$1/renderUVSL
 python renderImgMappedSL.py --sceneId $1 --task $2 --vId $3
 
 #python3 fromHDRtoLDR.py --sceneId $1 --rgbMode $2 --matMode $3 --maskMode mmap
