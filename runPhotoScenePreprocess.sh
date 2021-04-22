e00caf09bf546a6ec15cb701b36e708e0d053b5d
yuyingyeh <yuying1423@gmail.com>
2021-04-22 02:14:52 -0700

    add runPhotoScenePreprocess.sh

---
+++ runPhotoScenePreprocess.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+pip install opencv-python==3.4.2.17
+pip install joblib
+pip install imageio==2.6
+
+cd /eccv20dataset/yyeh/material-preprocess
+python script_extractScanNet.py --sceneId $1 --machine cluster
+
+pip3 install tqdm
+pip3 install scikit-learn
+
+python script_preprocess.py --sceneId $1 --machine cluster
+bash script_runInvRender.sh $1 0 "cluster"
+
+pip3 install torch --upgrade
+python script_preprocess2.py --sceneId $1 --machine cluster --renderDefault
--- runRenderImg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-apt-get update
-apt-get -y install libopencv-dev
-cd /siggraphasia20dataset/code/Routine/DatasetCreation/material
-python renderImgSN.py --sceneId $1 --forceOutput
-pip3 install tqdm opencv-python==3.4.2.17
-python3 fromHDRtoLDR.py --sceneId $1 --rgbMode imscannet --matMode _
