1fe910af5ec627c2b6a21ae9dc75cea48e4b008e
yuyingyeh <yuying1423@gmail.com>
2021-05-06 03:59:10 -0700

    update runPhotoScenePreprocess.sh

--- runPhotoScenePreprocess.sh
+++ runPhotoScenePreprocess.sh
@@ -18,3 +18,19 @@ bash script_runInvRender.sh $1 0 "cluster"
 
 pip3 install torch --upgrade
 python script_preprocess2.py --sceneId $1 --machine cluster --renderDefault --renderWhite
+
+# Need to first get selectedGraphDict.txt
+# Graph Classification
+orSnRoot="/eccv20dataset/yyeh/OpenRoomScanNetView"
+scene="scene$sceneId"
+if [ ! -s "$orSnRoot/$scene/selectedGraphDict.txt" ]
+then
+    #cd $preprocessRoot
+    #python genMatClsList.py --sceneId $sceneId --machine cluster
+    cd $preprocessRoot
+    bash script_runGraphClassifier.sh $sceneId $gpuId cluster
+    echo "New graph dict saved at $orSnRoot/$scene/selectedGraphDict.txt !"
+else
+    echo "$orSnRoot/$scene/selectedGraphDict.txt exists! Skip!"
+fi
+python script_preprocess2.py --sceneId $1 --machine cluster --renderAssignedMatch --renderAssignedGan
